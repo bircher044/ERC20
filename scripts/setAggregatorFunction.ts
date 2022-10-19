525e6138b944da02d9aab6c9da786675a40f9f62
Oleksii Bereza <95891690+bircher044@users.noreply.github.com>
2022-10-20 02:10:32 +0300

    changed contract address after redeploy

--- scripts/setAggregatorFunction.ts
+++ scripts/setAggregatorFunction.ts
@@ -2,7 +2,7 @@ import {ethers} from "hardhat";
 import {USDO__factory} from "../typechain-types";
 import * as dotenv from 'dotenv';
 
-const USDOContractAddress = "0x2c4905771771349DdBb416c03f83de65eF510553";
+const USDOContractAddress = "0xaAAa5D9E2AE260C9f1FdF9c4204C26a7C8a39525";
 
 const aggregatorAddress = "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e";
 
