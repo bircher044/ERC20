34c81a47f4e0be072efa1a9085caf645e871d842
Oleksii Bereza <95891690+bircher044@users.noreply.github.com>
2022-10-19 19:50:25 +0300

    added requier for positive balance to withdraw

--- contracts/USDO.sol
+++ contracts/USDO.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "hardhat/console.sol";
 
 contract USDO is ERC20, Ownable {
     
@@ -16,6 +17,7 @@ contract USDO is ERC20, Ownable {
 
     constructor() ERC20("USDO", "USD"){
         _mint(address(this), 10 ** 9);
+        transferOwnership(msg.sender);
     }
     
     function setAggregator(address _newEthAggregator) public onlyOwner {
@@ -29,21 +31,26 @@ contract USDO is ERC20, Ownable {
     }
     
     function withdraw() public onlyOwner{
+        require(address(this).balance > 0, "USDO: zero balance");
+
         uint256 amount = address(this).balance;
         payable(msg.sender).transfer(amount);
 
         emit withdrawed(msg.sender, amount);
     }
 
-    function buy() payable  public{
-        require(ethAggregator != address(0), "USDO contract is on pause");
+    receive() external payable{
+        require(ethAggregator != address(0), "USDO: contract is on pause");
 
         AggregatorV3Interface oracle = AggregatorV3Interface(ethAggregator);
 
         (, int256 answer,,,) = oracle.latestRoundData();
 
         uint256 tokenAmount = (msg.value * uint256(answer)) / (10 ** decimals());
-        transfer(msg.sender, tokenAmount);
+        
+        require(balanceOf(address(this)) >= tokenAmount, "USDO: not enought tokens left");
+
+        _transfer(address(this), msg.sender, tokenAmount);
 
         emit tokenSold(msg.sender, tokenAmount);
     }
