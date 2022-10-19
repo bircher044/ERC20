458442859901f32270f091bafe57cc7aa1139745
Oleksii Bereza <95891690+bircher044@users.noreply.github.com>
2022-10-19 14:46:32 +0300

    function getAggregator added

--- contracts/USDO.sol
+++ contracts/USDO.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
@@ -22,6 +22,10 @@ contract USDO is ERC20, Ownable {
         ethAggregator = _newEthAggregator;
 
         emit aggregatorChanged(ethAggregator);
+    }
+
+    function getAggregator() public view returns (address){
+        return ethAggregator;
     }
     
     function withdraw() public onlyOwner{
